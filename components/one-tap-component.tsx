7e983754bdb6fe03675b4e5684ee75ea8e5f5e17
dmb-dev87 <dmbdev800@gmail.com>
2023-11-07 12:28:22 -0500

    add google one tap login feature

--- components/one-tap-component.tsx
+++ components/one-tap-component.tsx
@@ -3,16 +3,16 @@
 import useOneTapSignin from "hooks/useOneTapSignin";
 
 export function OneTapComponent() {
-    const { isLoading: oneTapIsLoading } = useOneTapSignin({
-        redirect: false,
-        parentConatinerId: "oneTap",
-    })
+  const { isLoading: oneTapIsLoading } = useOneTapSignin({
+    redirect: false,
+    parentConatinerId: "oneTap",
+  })
 
-    return (
-        <div
-            id="oneTap"
-            className="flex flex-col"
-            style={{ position: "absolute", right: "0" }}
-        />
-    );
+  return (
+    <div
+      id="oneTap"
+      className="flex flex-col"
+      style={{ position: "absolute", right: "0" }}
+    />
+  );
 };
--- hooks/useOneTapSignin.ts
+++ hooks/useOneTapSignin.ts
@@ -2,54 +2,54 @@ import { useEffect, useState } from "react";
 import { useSession, signIn, SignInOptions } from "next-auth/react";
 
 interface OneTapSigninOptions {
-    parentConatinerId?: string;
+  parentConatinerId?: string;
 }
 
 const useOneTapSignin = (
-    options?: OneTapSigninOptions & Pick<SignInOptions, 'redirect' | 'callbackUrl'>
+  options?: OneTapSigninOptions & Pick<SignInOptions, 'redirect' | 'callbackUrl'>
 ) => {
-    const { parentConatinerId } = options || {};
-    const [isLoading, setIsLoading] = useState(false);
+  const { parentConatinerId } = options || {};
+  const [isLoading, setIsLoading] = useState(false);
 
-    const { status } = useSession({
-        required: true,
-        onUnauthenticated() {
-            if (!isLoading) {
-                const { google } = window;
-                if (google) {
-                    console.log(google);
-                    google.accounts.id.initialize({
-                        client_id: "50117126583-fkgi6uqq92vpmqnc30u8hvlkmsrui1pg.apps.googleusercontent.com",
-                        callback: async (response: any) => {
-                            setIsLoading(true);
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      if (!isLoading) {
+        const { google } = window;
+        if (google) {
+          console.log(google);
+          google.accounts.id.initialize({
+            client_id: "50117126583-fkgi6uqq92vpmqnc30u8hvlkmsrui1pg.apps.googleusercontent.com",
+            callback: async (response: any) => {
+              setIsLoading(true);
 
-                            await signIn('google', { callbackUrl: '/' })
+              await signIn('google', { callbackUrl: '/' })
 
-                            // await signIn("googleonetap", {
-                            //     credential: response.credential,
-                            //     redirect: true,
-                            //     ...options,
-                            // });
-                            setIsLoading(false);
-                        },
-                        prompt_parent_id: parentConatinerId,
-                    });
+              // await signIn("googleonetap", {
+              //     credential: response.credential,
+              //     redirect: true,
+              //     ...options,
+              // });
+              setIsLoading(false);
+            },
+            prompt_parent_id: parentConatinerId,
+          });
 
-                    google.accounts.id.prompt((notification: any) => {
-                        if (notification.isNotDisplayed()) {
-                            console.log("getNotDisplayedReason ::", notification.getNotDisplayedReason());
-                        } else if (notification.isSkippedMoment()) {
-                            console.log("getSkippedReason  ::", notification.getSkippedReason());
-                        } else if (notification.isDismissedMoment()) {
-                            console.log("getDismissedReason ::", notification.getDismissedReason());
-                        }
-                    });
-                }
+          google.accounts.id.prompt((notification: any) => {
+            if (notification.isNotDisplayed()) {
+              console.log("getNotDisplayedReason ::", notification.getNotDisplayedReason());
+            } else if (notification.isSkippedMoment()) {
+              console.log("getSkippedReason  ::", notification.getSkippedReason());
+            } else if (notification.isDismissedMoment()) {
+              console.log("getDismissedReason ::", notification.getDismissedReason());
             }
-        },
-    });
+          });
+        }
+      }
+    },
+  });
 
-    return { isLoading };
+  return { isLoading };
 };
 
 export default useOneTapSignin;
